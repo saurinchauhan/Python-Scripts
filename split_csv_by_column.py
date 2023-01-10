4659f00bef79acb2b548da1d8935a9f8fb33d218
Saurin Chauhan <saurin.chauhan@gmail.com>
2023-01-10 22:53:49 +0530

    Added comments

--- split_csv_by_column.py
+++ split_csv_by_column.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
+# read csv
 df = pd.read_csv('datafile.csv')
 
+# group data by 'Area' and loop the data
 for area,group in df.groupby(['Area']):
     print(area)
+    # create new csv based on grouped data
     group.to_csv(f'datafile_{area}.csv')
 
 for state,group in df.groupby(['State/UT']):
